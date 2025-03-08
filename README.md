1a9b59fd165c0886bd9ba50428b06276ce9b7258
jameskeywood <jskeywood@gmail.com>
2025-03-08 11:41:34 +0000

    Initial commit, simple bash script with little error handling

---
+++ README.md
@@ -0,0 +1,20 @@
+# Study
+Tool to track focused minutes of study.
+
+## Running
+To run the program, execute:
+```
+./study.sh start
+```
+or
+```
+./study.sh view
+```
+
+## Sources
+- gnuplot: http://www.gnuplot.info/
+- Using trap in Bash: https://stackoverflow.com/questions/46816904/bash-break-out-of-loop-with-ctrl-c-but-continue-with-script
+- Suppressing terminal output in Bash: https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
+- Timing in Bash: https://stackoverflow.com/questions/65348480/time-part-of-a-script-running-time
+- Plotting graphs with gnuplot: https://www.xmodulo.com/plot-bar-graph-gnuplot.html
+
---
+++ study.sh
@@ -0,0 +1,38 @@
+if [ $1 == "start" ]
+then
+	s_date=$(date "+%d%m%y")
+	s_start=$(date "+%s")
+
+	echo "$s_date"
+
+	trap 's_end=$(date "+%s");
+	  	  s_secs=$(bc <<< "$s_end - $s_start");
+		  s_mins=$(bc <<< "$s_secs / 60");
+
+		  # combine minutes if date the same
+		  prev_date=$(tail -n 1 times.csv | cut -d, -f1)
+		  if [ $s_date == $prev_date ]
+		  then
+			  prev_mins=$(tail -n 1 times.csv | cut -d, -f2)
+			  s_mins=$(bc <<< "$s_mins + $prev_mins")
+
+			  # delete last line
+			  sed -i "$ d" times.csv
+		  fi
+
+
+		  echo "$s_date,$s_mins" >> times.csv;
+          echo "Saved: $s_date,$s_mins";
+		  exit' SIGINT
+
+	sox -n -d synth -1 brownnoise vol 0.02 &> /dev/null
+elif [ $1 == "view" ]
+then
+	gnuplot -e 'set datafile separator ",";
+				set title "Focused study minutes per day";
+				set terminal dumb enhanced ansi fillchar "-";
+				set style data histograms;
+				set style fill solid;
+				set yrange [0:*];
+				plot "times.csv" using 2:xtic(1) notitle;'
+fi
